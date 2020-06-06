93a939d3eec44c5c034d6d4acd3788d9a12cb7f3
kawada <vgnsl94@gmail.com>
2020-06-06 17:22:12 +0900

    編集、削除機能実装

---
+++ laravel/resources/views/articles/edit.blade.php
@@ -0,0 +1,35 @@
+@extends('app')
+
+@section('title', '記事更新')
+
+@section('content')
+@include('nav')
+
+<div class="card card-image" style="background-image: url(https://i.pinimg.com/originals/23/52/75/235275027f4f716ae1b0e722ee9b3194.png);">
+  <div class="text-white text-center rgba-stylish-strong py-5 px-4">
+    <div class="py-5">
+      <div class="container">
+        <h2 class="card-title h2 my-4 py-2">記事更新</h2>
+        <div class="card">
+
+          <div class="card-body">
+            @include('error_card_list')
+            <p class="card-text">
+              <form class="text-center" style="color: #757575;" action="{{ route('articles.update', ['article' => $article->id]) }}" method="POST">
+                @method('PATCH')
+                @include('articles.form')
+                <button class="btn purple-gradient btn-block" type="submit">更新する</button>
+
+              </form>
+            </p>
+
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
--- laravel/resources/views/articles/form.blade.php
+++ laravel/resources/views/articles/form.blade.php
@@ -1,10 +1,10 @@
 @csrf
 <div class="md-form">
-  <input type="text" id="title" class="form-control" name="title" required value="{{ old('title') }}">
+  <input type="text" id="title" class="form-control" name="title" required value="{{ $article->title ?? old('title') }}">
   <label for="title">タイトル</label>
 </div>
 
 <div class="md-form">
-  <textarea id="body" class="md-textarea form-control" rows="5" name="body" required></textarea>
+  <textarea id="body" class="md-textarea form-control" rows="5" name="body" required >{{ $article->body ?? old('body') }}</textarea>
   <label for="body">本文</label>
 </div>
