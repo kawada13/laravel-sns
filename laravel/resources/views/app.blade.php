641b991d9655615952d0f9d9f52a795175e1d590
kawada <vgnsl94@gmail.com>
2020-06-08 15:14:38 +0900

    いいね機能実装

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -16,9 +17,12 @@
 </head>
 
 <body>
+  <div id="app">
+    @yield('content')
+  </div>
 
-  @yield('content')
 
+  <script src="{{ mix('js/app.js') }}"></script>
   <!-- JQuery -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
--- laravel/resources/views/articles/card.blade.php
+++ laravel/resources/views/articles/card.blade.php
@@ -1,4 +1,3 @@
-
 <div class="card mt-3 example z-depth-5">
 
   <div class="card-body d-flex flex-row">
@@ -6,60 +5,60 @@
     <i class="fas fa-chess-knight fa-3x mr-2"></i>
     <div>
 
-        <div class="font-weight-bold">
-          {{ $article->user->name }}
+      <div class="font-weight-bold">
+        {{ $article->user->name }}
+      </div>
+      <div class="font-weight-lighter">
+        {{ $article->created_at->format('Y/m/d H:i') }}
+      </div>
+    </div>
+    @if( Auth::id() === $article->user_id )
+    <!-- dropdown -->
+    <div class="ml-auto card-text">
+      <div class="dropdown">
+        <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <button type="button" class="btn btn-link text-muted m-0 p-2">
+            <i class="fas fa-concierge-bell"></i>
+          </button>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right">
+          <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
+            <i class="fas fa-pen mr-1"></i>記事を更新する
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
+            <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+          </a>
         </div>
-        <div class="font-weight-lighter">
-         {{ $article->created_at->format('Y/m/d H:i') }}
-        </div>
-     </div>
-     @if( Auth::id() === $article->user_id )
-        <!-- dropdown -->
-        <div class="ml-auto card-text">
-          <div class="dropdown">
-            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <button type="button" class="btn btn-link text-muted m-0 p-2">
-              <i class="fas fa-concierge-bell"></i>
-              </button>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right">
-              <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
-                <i class="fas fa-pen mr-1"></i>記事を更新する
-              </a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
-                <i class="fas fa-trash-alt mr-1"></i>記事を削除する
-              </a>
-            </div>
-          </div>
-        </div>
-        <!-- dropdown -->
+      </div>
+    </div>
+    <!-- dropdown -->
 
-        <!-- modal -->
-        <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                  <span aria-hidden="true">×</span>
-                </button>
-              </div>
-              <form method="POST" action="{{ route('articles.destroy', ['article' => $article->id]) }}">
-                @csrf
-                @method('DELETE')
-                <div class="modal-body">
-                  {{ $article->title }}を削除するで。ほんまにええか？
-                </div>
-                <div class="modal-footer justify-content-between">
-                  <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                  <button type="submit" class="btn btn-danger">削除する</button>
-                </div>
-              </form>
-            </div>
+    <!-- modal -->
+    <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+              <span aria-hidden="true">×</span>
+            </button>
           </div>
+          <form method="POST" action="{{ route('articles.destroy', ['article' => $article->id]) }}">
+            @csrf
+            @method('DELETE')
+            <div class="modal-body">
+              {{ $article->title }}を削除しますがよろしいですか？
+            </div>
+            <div class="modal-footer justify-content-between">
+              <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+              <button type="submit" class="btn btn-danger">削除する</button>
+            </div>
+          </form>
         </div>
-        <!-- modal -->
-      @endif
+      </div>
+    </div>
+    <!-- modal -->
+    @endif
 
   </div>
 
@@ -68,6 +67,17 @@
         {{ $article->title }}
       </a></h4>
     <p class="card-text">{{ $article->body }}</p>
-   </div>
+  </div>
+  <div class="card-body pt-0 pb-2 pl-3">
+    <div class="card-text">
+    <article-like
+        :initial-is-liked-by='@json($article->isLikedBy(Auth::user()))'
+        :initial-count-likes='@json($article->count_likes)'
+        :authorized='@json(Auth::check())'
+        endpoint="{{ route('articles.like', ['article' => $article]) }}"
+      >
+      </article-like>
+    </div>
+  </div>
 
 </div>
